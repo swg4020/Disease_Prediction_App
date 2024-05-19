5530f23fe9e8109d691a62f668a5943ec22d5101
swg4020 <swg4020@naver.com>
2024-05-19 11:33:10 +0900

    필요 라이브러리 설치, api 설정

---
+++ src/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const apiKey =
+  "uzqzCV0OaR8IQR7UXErSXf++hcUKyQRCS2CT6q/LrQdoMNHPHkm3UtU5godlE8EyQMoTu5sY1nkbJKMWOouO6w==";
+
+// 해수욕장 정보
+const instanc = axios.create({
+  baseURL: "http://apis.data.go.kr/B550928/dissForecastInfoSvc/",
+  params: {
+    serviceKey: apiKey,
+    pageNo: 1,
+    numOfRows: 10,
+    type: `JSON`,
+  },
+});
+
+export const baseData = ({ queryKey }) => {
+    const [getDissForecastInfo, diss, sido] = queryKey;
+    return instanc
+      .get(`${getDissForecastInfo}?&dissCd=${diss}&znCd=${sido}`)
+      .then((res) => res.data);
+  };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,16 @@
+import { useState } from "react";
+import { baseData } from "../../api";
+import { useQuery } from "@tanstack/react-query";
+
 export const Home = () => {
+  const [diss, setDiss] = useState(1);
+  const [sido, setSido] = useState(11);
+
+  const bsasInfo = useQuery({
+    queryKey: ["getDissForecastInfo", diss, sido],
+    queryFn: baseData,
+  });
+
+  console.log(bsasInfo);
   return <div></div>;
 };
