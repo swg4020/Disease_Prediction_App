8ca38ea40264a2a30c0e6f370c4f9f0af3d8c288
swg4020 <swg4020@naver.com>
2024-05-29 17:01:40 +0900

    질병 리스트, home 기능 추가중

---
+++ src/components/listUpD.js
@@ -0,0 +1,26 @@
+export const listUpD = [
+  {
+    id: 1,
+    title: "감기",
+  },
+  {
+    id: 2,
+    title: "눈병",
+  },
+  {
+    id: 3,
+    title: "식중독",
+  },
+  {
+    id: 4,
+    title: "천식",
+  },
+  {
+    id: 5,
+    title: "피부염",
+  },
+  {
+    id: 15,
+    title: "심뇌혈관 질환",
+  },
+];
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,28 +3,41 @@ import { baseData } from "../../api";
 import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 import { listUp } from "../../components/listUp";
+import { listUpD } from "../../components/listUpD";
 
 const Container = styled.div``;
 const ConWrap = styled.div``;
+const Con = styled.div``;
 
 export const Home = () => {
   const [diss, setDiss] = useState(1);
-  const [sido, setSido] = useState(11);
+  const [sido, setSido] = useState(26);
 
   const bsasInfo = useQuery({
     queryKey: ["getDissForecastInfo", diss, sido],
     queryFn: baseData,
   });
 
-  const data = bsasInfo && bsasInfo?.data?.response?.body?.items
-  console.log(data);
-  const info = listUp.filter(res => res.id == sido);
-  console.log(info[0]?.title)
+  const basedata = bsasInfo && bsasInfo?.data?.response?.body?.items;
+  console.log(basedata);
+  const info = listUp.filter((res) => res.id == sido);
+  const infoD = listUpD.filter((res) => res.id === diss);
+  const titleInfo = info[0].title;
+  const titleDInfo = infoD[0].title;
+  console.log(titleInfo);
+  console.log(titleDInfo);
 
-  
   return (
     <Container>
-      <ConWrap></ConWrap>
+      <ConWrap>
+        <Con>
+          <h2>예측 지역 : {titleInfo}</h2>
+          <h2>예측 질병 : {titleDInfo}</h2>
+          <h3>예측 일자 : {basedata[0].dt}</h3>
+          <p>질병 예측 진료건수  : {basedata[0].cnt}</p>
+          <p></p>
+        </Con>
+      </ConWrap>
     </Container>
   );
 };
